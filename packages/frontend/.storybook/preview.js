24d1c5703c6abb402e4c658720011ed77089e172
Andrew Luca <thendrluca@gmail.com>
2021-05-02 02:02:21 +0300

    chore: integrate tailwind in storybook

--- packages/frontend/.storybook/preview.js
+++ packages/frontend/.storybook/preview.js
@@ -1,4 +1,5 @@
 import { addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import 'tailwindcss/tailwind.css';
 
 addDecorator(withKnobs);
--- packages/frontend/.storybook/webpack.config.js
+++ packages/frontend/.storybook/webpack.config.js
@@ -77,6 +77,19 @@ module.exports = async ({ config, mode }) => {
           ],
         },
       ],
+    },
+    {
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+        },
+      ],
     }
   );
 
