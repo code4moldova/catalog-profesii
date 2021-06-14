43fe41b39e1a0067040847a8d8179ecd9cda3f2e
ion-golovco <gaminglions2@gmail.com>
2021-06-14 23:59:56 +0300

    feat: add ExternalCard component

---
+++ packages/frontend/components/external-card/external-card.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react';
+import {
+  ExternalCard as Component,
+  ExternalCardProps,
+} from './external-card.component';
+
+export default {
+  title: 'External Card',
+  component: Component,
+  args: {
+    href: '#',
+    header: 'Marketing',
+    icon: 'https://picsum.photos/seed/external-card/512/512',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+  } as ExternalCardProps,
+} as Meta;
+
+export const ExternalCard: Story<ExternalCardProps> = (args) => (
+  <Component {...args} />
+);
