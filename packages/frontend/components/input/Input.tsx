cede19f307d32ec9f319e57364284396c484d805
Radu Cazacu <cazacuraduic@gmail.com>
2021-06-17 15:38:39 +0300

    Input

---
+++ packages/frontend/components/input/Input.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from '../grid/container.component';
+
+const Input = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Căutare profesie" />
+    </div>
+  );
+};
+
+export default Input;
