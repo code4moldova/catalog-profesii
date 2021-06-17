a7a9eb8b69fdcbcf1d6020591ce6e51744532aec
Radu Cazacu <cazacuraduic@gmail.com>
2021-06-17 15:52:44 +0300

    Feat: add input component

---
+++ packages/frontend/components/input/Input.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta, Story } from '@storybook/react';
+import InputComponent from './Input';
+
+export default {
+  title: 'Input',
+} as Meta;
+
+export const Input: Story = () => <InputComponent />;
--- packages/frontend/components/input/Input.tsx
+++ packages/frontend/components/input/Input.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Container } from '../grid/container.component';
+import searchIcon from './assets/searchIcon.svg';
 
 const Input = () => {
   return (
-    <div>
-      <input type="text" placeholder="Căutare profesie" />
+    <div className="flex rounded-md border-2">
+      <input
+        type="text"
+        placeholder="Căutare profesie"
+        className="border-none outline-none w-72 py-2.5 pl-4 pr-3 "
+      />
+      <img
+        src={searchIcon}
+        alt=""
+        style={{ marginLeft: 'auto', marginRight: '1rem' }}
+      />
     </div>
   );
 };
