77b49974a512aa3e2c4977770cabec59bd07f9a3
Raduc4 <55291600+Raduc4@users.noreply.github.com>
2021-07-03 19:59:20 +0300

    feat: add ArrowLink component (#63)
    * Feat: add arrow link
    
    * make adjustments
    
    Co-authored-by: Andrew Luca <thendrluca@gmail.com>

---
+++ packages/frontend/components/arrow-link/arrow-link.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react';
+import { ArrowLink as Component } from './arrow-link';
+
+export default {
+  title: 'Components / Arrow Link',
+  component: Component,
+} as Meta;
+
+export const ArrowLink: Story = () => (
+  <div className="grid grid-cols-2 gap-3 text-blue-500">
+    <Component href="#">Front-end Developer</Component>
+    <Component href="#">Backend-end Developer</Component>
+    <Component href="#">Full-Stack Developer</Component>
+    <Component href="#">Web Administrator</Component>
+    <Component href="#">Designer grafic</Component>
+    <Component href="#">UX Designer</Component>
+    <Component href="#">UI Designer</Component>
+    <Component href="#">Illustrator</Component>
+  </div>
+);
