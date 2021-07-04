9fb25925b46197499868f829e95ada23695213ce
Andrew Luca <thendrluca@gmail.com>
2021-07-04 23:34:40 +0300

    fix: move font from app to document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,14 +9,6 @@ import { Footer } from '../sections/footer/footer.section';
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Welcome to frontend!</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
-        />
-      </Head>
       <Header />
       <main>
         <Component {...pageProps} />
---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Catalog Profesii!</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
