9720c6b7ad5aa908e4686f321cad1bdc7c2e8773
Sergiu <synthezys@gmail.com>
2021-07-05 21:20:39 +0300

    feat: Added Collapsable Panel

---
+++ components/panel/panel.component.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+
+export type PanelProps = {
+  title: string;
+  collapsable: boolean;
+  children: string;
+};
+
+export function Panel(props: PanelProps) {
+  const { title, collapsable = true, children } = props;
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <section>
+      <div
+        onClick={() => setCollapsed(!collapsed)}
+        className="flex justify-between items-center py-4 px-5 border-b border-gray-200 "
+      >
+        <h3 className="text-xl font-medium leading-none">{title}</h3>
+        {collapsable && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="13"
+            viewBox="0 0 20 13"
+            fill="none"
+            className={clsx(
+              'transform transition-transform duration-300 ease-in-out',
+              !collapsed && 'rotate-180',
+              collapsed && 'rotate-90'
+            )}
+          >
+            <path
+              d="M2.8742e-08 10.5897L2.35 13L10 5.17094L17.65 13L20 10.5897L10 0.333332L2.8742e-08 10.5897Z"
+              fill="black"
+            />
+          </svg>
+        )}
+      </div>
+      <div
+        className={clsx(
+          'p-4 transform origin-top transition-transform duration-500 ease-in-out overflow-hidden',
+          collapsed && 'scale-y-0',
+          !collapsed && 'scale-y-1'
+        )}
+      >
+        {children}
+      </div>
+    </section>
+  );
+}
---
+++ components/panel/panel.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react';
+import { Panel as Component, PanelProps } from './panel.component';
+
+export default {
+  title: 'Components / Collapsable Panel',
+  component: Component,
+  args: {
+    title: 'Solicită ajutor de la profesioniști',
+    collapsable: true,
+  } as PanelProps,
+} as Meta;
+
+export const Panel: Story<PanelProps> = (args) => (
+  <Component {...args}>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic nam soluta
+    tenetur unde. Architecto dolore eveniet excepturi voluptatem! Ab beatae
+    debitis inventore modi, officiis porro quam quibusdam similique totam
+    veritatis.
+  </Component>
+);
