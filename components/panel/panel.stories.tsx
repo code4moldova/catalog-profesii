d673f86af186f11a310d8251e2db905574fcd7bf
Sergiu Ravliuc <synthezys@gmail.com>
2021-07-05 23:38:57 +0300

    feat: add collapsable panel component (#70)
    Co-authored-by: Andrew Luca <thendrluca@gmail.com>

---
+++ components/panel/panel.stories.tsx
@@ -0,0 +1,62 @@
+import { Meta, Story } from '@storybook/react';
+import { Panel as Component, PanelProps } from './panel.component';
+
+export default {
+  title: 'Components / Collapsable Panel',
+  component: Component,
+  args: {
+    title: 'Solicită ajutor de la profesioniști',
+    collapsable: true,
+  },
+} as Meta<PanelProps>;
+
+export const Panel: Story<PanelProps> = (args) => (
+  <>
+    <Component {...args}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic nam soluta
+      tenetur unde. Architecto dolore eveniet excepturi voluptatem! Ab beatae
+      debitis inventore modi, officiis porro quam quibusdam similique totam
+      veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+      nam soluta tenetur unde. Architecto dolore eveniet excepturi voluptatem!
+      Ab beatae debitis inventore modi, officiis porro quam quibusdam similique
+      totam veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Hic nam soluta tenetur unde. Architecto dolore eveniet excepturi
+      voluptatem! Ab beatae debitis inventore modi, officiis porro quam
+      quibusdam similique totam veritatis. Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit. Hic nam soluta tenetur unde. Architecto
+      dolore eveniet excepturi voluptatem! Ab beatae debitis inventore modi,
+      officiis porro quam quibusdam similique totam veritatis. Lorem ipsum dolor
+      sit amet, consectetur adipisicing elit. Hic nam soluta tenetur unde.
+      Architecto dolore eveniet excepturi voluptatem! Ab beatae debitis
+      inventore modi, officiis porro quam quibusdam similique totam veritatis.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic nam soluta
+      tenetur unde. Architecto dolore eveniet excepturi voluptatem! Ab beatae
+      debitis inventore modi, officiis porro quam quibusdam similique totam
+      veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+      nam soluta tenetur unde. Architecto dolore eveniet excepturi voluptatem!
+      Ab beatae debitis inventore modi, officiis porro quam quibusdam similique
+      totam veritatis.
+    </Component>
+
+    <Component {...args}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic nam soluta
+      tenetur unde. Architecto dolore eveniet excepturi voluptatem! Ab beatae
+      debitis inventore modi, officiis porro quam quibusdam similique totam
+      veritatis.
+    </Component>
+
+    <Component {...args}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic nam soluta
+      tenetur unde. Architecto dolore eveniet excepturi voluptatem! Ab beatae
+      debitis inventore modi, officiis porro quam quibusdam similique totam
+      veritatis.
+    </Component>
+
+    <Component {...args}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic nam soluta
+      tenetur unde. Architecto dolore eveniet excepturi voluptatem! Ab beatae
+      debitis inventore modi, officiis porro quam quibusdam similique totam
+      veritatis.
+    </Component>
+  </>
+);
