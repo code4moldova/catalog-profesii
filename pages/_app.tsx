ddba0d332c646433f1c5eb244f43a8d9a30b843f
Andrew Luca <thendrluca@gmail.com>
2021-07-16 01:06:54 +0300

    feat: add apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import possibleTypes from '../generated/possible-types';
 import '../tailwind.css';
-
 import { Header } from '../sections/header/header.section';
 import { Footer } from '../sections/footer/footer.section';
 
+const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const environment = process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT;
+const token = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
+
+const client = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${space}/environments/${environment}`,
+  headers: { Authorization: `Bearer ${token}` },
+  cache: new InMemoryCache({ ...possibleTypes }),
+});
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Head>
         <title>Catalog Profesii!</title>
       </Head>
@@ -18,7 +29,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         </main>
         <Footer />
       </div>
-    </>
+    </ApolloProvider>
   );
 }
 
