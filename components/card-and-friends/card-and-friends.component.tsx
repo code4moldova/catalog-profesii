8eda5cd594a9dc1031dd744a61df2b14a89a430a
Radu Cazacu <cazacuraduic@gmail.com>
2021-07-22 18:47:24 +0300

    Feat: small adjustments

--- components/card-and-friends/card-and-friends.component.tsx
+++ components/card-and-friends/card-and-friends.component.tsx
@@ -32,22 +32,28 @@ export const CardAndFriends = ({
         <h3 className="text-sm mb-2.5">{title}</h3>
         <p className="font-light text-xs">{description}</p>
       </div>
-      <div className="px-4 py-2.5">
-        <h4>{caption_title}</h4>
-        <ul>
+      <div className="px-4 py-2.5 bg-gray-100">
+        <h4 className="font-medium text-sm text-blue-500 mb-2.5">
+          {caption_title}
+        </h4>
+        <ul className="mb-2.5">
           {caption_list?.map((caption, index) => (
-            <li key={index}>{caption}</li>
+            <li key={index}>
+              ○ <span className="ml-1">{caption}</span>
+            </li>
           ))}
         </ul>
       </div>
-      <div className="flex flex-col">
-        <Button size="medium" color="blue" className="w-32">
+      <div className="flex flex-col justify-center items-center py-4">
+        <Button size="medium" color="blue">
           <div className="inline-flex items-center justify-center">
-            <a href={button_redirect_link}>{button_text}</a>
+            <a className="mr-2" href={button_redirect_link}>
+              {button_text}
+            </a>
             <Image src={image2} width={15} height={15} alt="" />
           </div>
         </Button>
-        <a className="text-center" href={`https://${host_platform_link}`}>
+        <a className="text-center mt-2" href={`https://${host_platform_link}`}>
           <span>{host_platform_link}</span>
         </a>
       </div>
